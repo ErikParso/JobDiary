311d796720c8f433207b7ac432f946b0741bd332
Erik Paro <parso@kros.sk>
2018-08-02 17:35:57 +0200

    - removed unused icons - themed icons - themed colors - black and blue theme initialised - line picture label redesign

---
+++ MyJobDiary Client/MyJobDiary/ThemeDictionaries/BlackAndBlue.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyJobDiary.ThemeDictionaries
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BlackAndBlue : ResourceDictionary
+    {
+        public BlackAndBlue()
+        {
+            InitializeComponent();
+        }
+    }
+}
