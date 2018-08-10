2530374ae6a0ae14cfa59adea5451a445a30cc98
Erik Paro <parso@kros.sk>
2018-08-10 15:54:58 +0200

    Display day of week in date pickers.

---
+++ MyJobDiary Client/MyJobDiary/View/DayOfWeekValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MyJobDiary.View
+{
+    public class DayOfWeekValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            switch (((DateTime)value).DayOfWeek)
+            {
+                case DayOfWeek.Monday: return "Po";
+                case DayOfWeek.Tuesday: return "Ut";
+                case DayOfWeek.Wednesday: return "St";
+                case DayOfWeek.Thursday: return "Št";
+                case DayOfWeek.Friday: return "Pi";
+                case DayOfWeek.Saturday: return "So";
+                case DayOfWeek.Sunday: return "Ne";
+                default: return null;
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+    }
+}
