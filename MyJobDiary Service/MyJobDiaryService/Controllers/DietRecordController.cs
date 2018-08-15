1c759a8367d50aee2e8657e8a38637a6657d752d
Erik Paro <parso@kros.sk>
2018-08-15 16:41:26 +0200

    Enabled migrations !

---
+++ MyJobDiary Service/MyJobDiaryService/Controllers/DietRecordController.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.Azure.Mobile.Server;
+using MyJobDiaryService.DataObjects;
+using MyJobDiaryService.Models;
+
+namespace MyJobDiaryService.Controllers
+{
+    [Authorize]
+    public class DietRecordController : TableController<DietRecord>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            MyJobDiaryContext context = new MyJobDiaryContext();
+            DomainManager = new EntityDomainManager<DietRecord>(context, Request);
+        }
+
+        // GET tables/TodoItem
+        public IQueryable<DietRecord> GetAllTodoItems()
+        {
+            string userId = GetUserId(User);
+            return Query().Where(d => d.UserId == userId);
+        }
+
+        // GET tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public SingleResult<DietRecord> GetTodoItem(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<DietRecord> PatchTodoItem(string id, Delta<DietRecord> patch)
+        {
+            return UpdateAsync(id, patch);
+        }
+
+        // POST tables/TodoItem
+        public async Task<IHttpActionResult> PostTodoItem(DietRecord item)
+        {
+            item.UserId = GetUserId(User);
+            DietRecord current = await InsertAsync(item);
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteTodoItem(string id)
+        {
+            return DeleteAsync(id);
+        }
+
+        private string GetUserId(IPrincipal user)
+        {
+            ClaimsPrincipal claimsUser = (ClaimsPrincipal)user;
+            string sid = claimsUser.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return sid;
+        }
+    }
+}
---
+++ MyJobDiary Service/MyJobDiaryService/Migrations/201808151433215_initDietRecordTable.cs
@@ -0,0 +1,113 @@
+namespace MyJobDiaryService.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.Infrastructure.Annotations;
+    using System.Data.Entity.Migrations;
+
+    public partial class initDietRecordTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.DietRecords",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128,
+                            annotations: new Dictionary<string, AnnotationValues>
+                            {
+                                {
+                                    "ServiceTableColumn",
+                                    new AnnotationValues(oldValue: null, newValue: "Id")
+                                },
+                            }),
+                        UserId = c.String(),
+                        StartLocation1 = c.String(),
+                        DestinationLocation1 = c.String(),
+                        ArrivalTime1 = c.DateTime(nullable: false),
+                        StartLocation2 = c.String(),
+                        DestinationLocation2 = c.String(),
+                        ArrivalTime2 = c.DateTime(nullable: false),
+                        Version = c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion",
+                            annotations: new Dictionary<string, AnnotationValues>
+                            {
+                                {
+                                    "ServiceTableColumn",
+                                    new AnnotationValues(oldValue: null, newValue: "Version")
+                                },
+                            }),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7,
+                            annotations: new Dictionary<string, AnnotationValues>
+                            {
+                                {
+                                    "ServiceTableColumn",
+                                    new AnnotationValues(oldValue: null, newValue: "CreatedAt")
+                                },
+                            }),
+                        UpdatedAt = c.DateTimeOffset(precision: 7,
+                            annotations: new Dictionary<string, AnnotationValues>
+                            {
+                                {
+                                    "ServiceTableColumn",
+                                    new AnnotationValues(oldValue: null, newValue: "UpdatedAt")
+                                },
+                            }),
+                        Deleted = c.Boolean(nullable: false,
+                            annotations: new Dictionary<string, AnnotationValues>
+                            {
+                                {
+                                    "ServiceTableColumn",
+                                    new AnnotationValues(oldValue: null, newValue: "Deleted")
+                                },
+                            }),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.CreatedAt, clustered: false);
+
+        }
+
+        public override void Down()
+        {
+            DropIndex("dbo.DietRecords", new[] { "CreatedAt" });
+            DropTable("dbo.DietRecords",
+                removedColumnAnnotations: new Dictionary<string, IDictionary<string, object>>
+                {
+                    {
+                        "CreatedAt",
+                        new Dictionary<string, object>
+                        {
+                            { "ServiceTableColumn", "CreatedAt" },
+                        }
+                    },
+                    {
+                        "Deleted",
+                        new Dictionary<string, object>
+                        {
+                            { "ServiceTableColumn", "Deleted" },
+                        }
+                    },
+                    {
+                        "Id",
+                        new Dictionary<string, object>
+                        {
+                            { "ServiceTableColumn", "Id" },
+                        }
+                    },
+                    {
+                        "UpdatedAt",
+                        new Dictionary<string, object>
+                        {
+                            { "ServiceTableColumn", "UpdatedAt" },
+                        }
+                    },
+                    {
+                        "Version",
+                        new Dictionary<string, object>
+                        {
+                            { "ServiceTableColumn", "Version" },
+                        }
+                    },
+                });
+        }
+    }
+}
