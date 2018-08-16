7d497a6b345fd3d909abbabaf3c03552ef4a93e6
Erik Paro <parso@kros.sk>
2018-08-16 16:32:39 +0200

    - project upgrade

---
+++ MyJobDiary Client/MyJobDiary/MyJobDiary.Android/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using MyJobDiary.Droid.Services;
+
+namespace MyJobDiary.Droid
+{
+    [Activity(Label = "MyJobDiary", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            App.SetLoginService(new LoginService(this));
+            App.SetLoadingService(new LoadingService(this));
+            App.SetDialogService(new DialogService(this));
+
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ MyJobDiary Client/MyJobDiary/MyJobDiary/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using MyJobDiary.Services;
+using MyJobDiary.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace MyJobDiary
+{
+    public partial class App : Application
+    {
+        public static ILoginService LoginService { get; private set; }
+        public static ILoadingService LoadingService { get; private set; }
+        public static IDialogService DialogService { get; private set; }
+
+        public App()
+        {
+            // The root page of your application
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        public static void SetLoginService(ILoginService loginService)
+        {
+            LoginService = loginService;
+        }
+
+        public static void SetLoadingService(ILoadingService loadingService)
+        {
+            LoadingService = loadingService;
+        }
+
+        public static void SetDialogService(IDialogService dialogService)
+        {
+            DialogService = dialogService;
+        }
+
+        protected override void OnStart()
+        {
+
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
