4d23e2af5f791fcd430cbd4a347718004c5e8b78
Erik Paro <parso@kros.sk>
2018-08-27 16:34:23 +0200

    - attendance error handling

--- MyJobDiary Client/MyJobDiary/MyJobDiary/UserControl/AttendanceItem.xaml.cs
+++ MyJobDiary Client/MyJobDiary/MyJobDiary/UserControl/AttendanceItem.xaml.cs
@@ -14,6 +14,8 @@ namespace MyJobDiary.UserControl
             "NoneColor", typeof(Color), typeof(AttendanceItem), Color.Transparent);
         public static readonly BindableProperty TransferColorProperty = BindableProperty.Create(
             "TransferColor", typeof(Color), typeof(AttendanceItem), Color.DarkGreen);
+        public static readonly BindableProperty ErrorColorProperty = BindableProperty.Create(
+            "ErrorColor", typeof(Color), typeof(AttendanceItem), Color.Red);
 
         public Color WorkColor
         {
@@ -33,6 +35,12 @@ namespace MyJobDiary.UserControl
             set { SetValue(TransferColorProperty, value); }
         }
 
+        public Color ErrorColor
+        {
+            get { return (Color)GetValue(ErrorColorProperty); }
+            set { SetValue(ErrorColorProperty, value); }
+        }
+
         public AttendanceItem()
         {
             InitializeComponent();
@@ -48,17 +56,32 @@ namespace MyJobDiary.UserControl
             double fullDay = TimeSpan.FromHours(24).TotalSeconds;
             TimeSpan counter = TimeSpan.FromHours(0);
 
-            foreach (var kp in day.Items)
+            try
             {
-                ColumnDefinition column = new ColumnDefinition();
-                column.Width = new GridLength(kp.Time.TotalSeconds / fullDay, GridUnitType.Star);
-                dayGrid.ColumnDefinitions.Add(column);
-                BoxView box = new BoxView();
-                box.BackgroundColor = GetColorBySection(kp.Section);
-                box.HorizontalOptions = LayoutOptions.Fill;
-                box.VerticalOptions = LayoutOptions.Fill;
-                dayGrid.Children.Add(box, dayGrid.ColumnDefinitions.Count - 1, 0);
-                counter += kp.Time;
+                foreach (var kp in day.Items)
+                {
+                    ColumnDefinition column = new ColumnDefinition();
+                    column.Width = new GridLength(kp.Time.TotalSeconds / fullDay, GridUnitType.Star);
+                    dayGrid.ColumnDefinitions.Add(column);
+                    BoxView box = new BoxView();
+                    box.BackgroundColor = GetColorBySection(kp.Section);
+                    box.HorizontalOptions = LayoutOptions.Fill;
+                    box.VerticalOptions = LayoutOptions.Fill;
+                    dayGrid.Children.Add(box, dayGrid.ColumnDefinitions.Count - 1, 0);
+                    counter += kp.Time;
+                }
+            }
+            catch
+            {
+                dayGrid.ColumnDefinitions.Clear();
+                dayGrid.Children.Clear();
+                dayGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                Label error = new Label();
+                error.Text = "chyba dochádzky";
+                error.HorizontalOptions = LayoutOptions.Center;
+                error.VerticalOptions = LayoutOptions.Center;
+                error.TextColor = ErrorColor;
+                dayGrid.Children.Add(error, 0, 0);
             }
         }
 
