4ee475cc0171271c51850d3b4812760b39f3998f
Erik Paro <parso@kros.sk>
2018-09-07 18:23:20 +0200

    - chart displaying worked time

--- MyJobDiary Client/MyJobDiary/MyJobDiary/View/StatistiscContentPage.xaml.cs
+++ MyJobDiary Client/MyJobDiary/MyJobDiary/View/StatistiscContentPage.xaml.cs
@@ -1,6 +1,11 @@
 ﻿
 using Microcharts;
+using MyJobDiary.Managers;
+using MyJobDiary.Model;
 using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,42 +17,34 @@ namespace MyJobDiary.View
 		public StatistiscContentPage ()
 		{
 			InitializeComponent();
-            var entries = new[]
-            {
-                new Microcharts.Entry(200)
-                {
-                    Label = "January",
-                    ValueLabel = "200",
-                    Color = SKColor.Parse("#0097E7"),
-                },
-                new Microcharts.Entry(400)
-                {
-                    Label = "February",
-                    ValueLabel = "400",
-                    Color = SKColor.Parse("#0097F7")
-                },
-                new Microcharts.Entry(-100)
-                {
-                    Label = "March",
-                    ValueLabel = "-100",
-                    Color = SKColor.Parse("#0097E7")
-                },
-                new Microcharts.Entry(100)
-                {
-                    Label = "April",
-                    ValueLabel = "100",
-                    Color = SKColor.Parse("#5597F7")
-                }
-            };
-            var chart = new BarChart() { Entries = entries };
-            chart.BackgroundColor = SKColor.Parse("#000000");
-            this.chartView.Chart = chart;
         }
 
-        protected override void OnAppearing()
+
+
+        protected async override void OnAppearing()
         {
             base.OnAppearing();
             NavigationPage.SetHasNavigationBar(this, false);
+
+            var shifts = await CachedTableManager<Shift>.Current.Value.GetAsync();
+            var chart = new BarChart()
+            {
+                Entries = Enumerable.Range(-12, 13)
+                    .Select(i => DateTime.Now.AddMonths(i).Date)
+                    .Select(d => (
+                        Date: d,
+                        TimeWorked: shifts.Where(s => s.TimeFrom.Year == d.Year && s.TimeFrom.Month == d.Month)
+                            .Sum(s => (s.TimeTo - s.TimeFrom).TotalHours)
+                    ))
+                    .Select(d => new Microcharts.Entry((float)d.TimeWorked)
+                    {
+                        Color = SKColor.Parse("#0097E7"),
+                        Label = d.Date.ToString("MMM"),
+                        ValueLabel = string.Format("{0:N2} H", d.TimeWorked),
+                    })
+            };
+            chart.BackgroundColor = SKColor.Parse("#000000");
+            this.chartView.Chart = chart;
         }
     }
 }
